79629b865b132da87766647e45447b5756c2745b
Rumit Tuladhar <thirdink@Rumits-MacBook-Pro.local>
2018-10-09 13:19:30 +0545

    sides with text and background done

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import Slides from '../components/Slides';
+
+const SLIDE_DATA = [
+  {
+    text: 'Welcome to JOBAPP',
+    color: '#03A9F4'
+  },
+  {
+    text: 'use this to get a job',
+    color: '#009688'
+  },
+  {
+    text: 'Set your location, then swipe away',
+    color: '#03A9F4'
+  }
+
+]
 
 export class WelcomeScreen extends React.Component {
   //   static navigationOptions = {
@@ -7,9 +24,7 @@ export class WelcomeScreen extends React.Component {
   // };
   render() {
     return (
-      <View>
-        <Text> WelcomeScreen </Text>
-      </View>
+        <Slides data={SLIDE_DATA}/>
     )
   }
 }
