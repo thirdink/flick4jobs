2724e5b581e3a431dff1879ea52fa0adcb171015
Rumit Tuladhar <thirdink@Rumits-MacBook-Pro.local>
2018-11-06 21:58:16 +0545

    completed the full course and push notifications working

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View ,Alert} from 'react-native';
 import { Provider } from 'react-redux';
+import registerForNotifications from './services/push_notifications'
 import store from './store'
-import { AppLoading, Asset, Font, Icon } from 'expo';
+import { Notifications } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
 
 export default class App extends React.Component {
-  
+  componentDidMount(){
+    registerForNotifications()
+    Notifications.addListener((notification)=>{
+
+      const{ data: { text },origin} = notification;
+
+      if(origin === 'received' && text ){
+      Alert.alert(
+        'New Push Notification',
+        text,
+        [{ text: 'OK. '}]
+      )
+      }
+    })
+
+  }
 
   render() {
 
